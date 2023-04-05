c48bedf22dc3bd6f92e9165af1d6fa21594c31ad
LRacine <racine.lea@icloud.com>
2023-04-05 11:36:47 +0100

    frontend intial

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+import { Inter } from 'next/font/google'
+
+export default function Home() {
+
+  return (
+    <>
+      <Head>
+        <title>Freespace</title>
+        <meta name="description" content="Find free spaces, for free, forever." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+
+      </main>
+    </>
+  )
+}
